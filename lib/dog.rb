8d3db8ea87856b8bf388cd86f35cf4e3bb897524
energetic-certificate-0804 <mjbugay@yahoo.com>
2020-08-29 21:43:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,17 @@
 class Dog 
+  
   def initialize(name)
-    
-  def name=(name)
     @name = name 
   end
+  
   def name 
     puts name
   end
   
-  def breed=(breed)
+  def initialize(breed)
     @breed = breed
   end
+  
   def breed
     puts breed
   end
