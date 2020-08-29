2693ad8a1a0f6c4d3720cc67fdb99c090884f422
energetic-certificate-0804 <mjbugay@yahoo.com>
2020-08-29 21:36:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,4 +3,15 @@ class Dog
     @dogs_name = dogs_name 
   end
   def name 
+    "#{dogs_name}"
+  end
+  
+  def breed=(dogs_breed)
+    @dogs_breed = dogs_breed
+  end
+  def breed
+    "#{dogs_breed}"
+  end
+  
+end
   
